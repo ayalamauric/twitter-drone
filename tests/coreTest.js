b0994c199e0893a55b6b3547fdec3e1fe877ab9f
redaxmedia <info@redaxmedia.com>
2019-02-24 15:14:18 +0100

    Add poor man unit testing

--- tests/coreTest.js
+++ tests/coreTest.js
@@ -1,46 +1,120 @@
+const expect = require('chai').expect;
+const exec = require('child_process').exec;
 const dotenv = require('dotenv');
-const drone = require('../');
-const core = drone.core;
-const spinner = drone.spinner;
-const option = drone.option;
-
-let CORE;
 
 beforeEach(() =>
 {
 	dotenv.config();
-	if (process.env.TWITTER_API_KEY &&
-		process.env.TWITTER_API_KEY_SECRET &&
-		process.env.TWITTER_ACCESS_TOKEN &&
-		process.env.TWITTER_ACCESS_TOKEN_SECRET
-	)
-	{
-		spinner.init();
-		option.init();
-		CORE = new core(
-		{
-			spinner,
-			option
-		});
-		CORE.init(
-		{
-			consumer_key: process.env.TWITTER_API_KEY,
-			consumer_secret: process.env.TWITTER_API_KEY_SECRET,
-			access_token: process.env.TWITTER_ACCESS_TOKEN,
-			access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-		});
-	}
-	else
-	{
-		process.exit(1);
-	}
 });
 
 describe('core', () =>
 {
-	it('destroy', done =>
+	it('search-tweet', done =>
 	{
-		done();
-		spinner.stop();
-	});
+		exec('bin/twitter-drone search tweet --query=test', (error, stdout) =>
+		{
+			expect(stdout).to.match(/tweetId/);
+			expect(stdout).to.match(/tweetText/);
+			expect(stdout).to.match(/userId/);
+			expect(stdout).to.match(/userName/);
+			done();
+		});
+	})
+	.timeout(5000);
+
+	it('search-user', done =>
+	{
+		exec('bin/twitter-drone search user --query=test', (error, stdout) =>
+		{
+			expect(stdout).to.match(/userId/);
+			expect(stdout).to.match(/userName/);
+			done();
+		});
+	})
+	.timeout(5000);
+
+	it('list-follower', done =>
+	{
+		exec('bin/twitter-drone list follower', (error, stdout) =>
+		{
+			expect(stdout).to.match(/userId/);
+			expect(stdout).to.match(/userName/);
+			done();
+		});
+	})
+	.timeout(5000);
+
+	it('list-friend', done =>
+	{
+		exec('bin/twitter-drone list friend', (error, stdout) =>
+		{
+			expect(stdout).to.match(/userId/);
+			expect(stdout).to.match(/userName/);
+			done();
+		});
+	})
+	.timeout(5000);
+
+	it('list-tweet', done =>
+	{
+		exec('bin/twitter-drone list tweet', (error, stdout) =>
+		{
+			expect(stdout).to.match(/tweetId/);
+			expect(stdout).to.match(/tweetText/);
+			done();
+		});
+	})
+	.timeout(5000);
+
+	it('list-like', done =>
+	{
+		exec('bin/twitter-drone list like', (error, stdout) =>
+		{
+			expect(stdout).to.match(/tweetId/);
+			expect(stdout).to.match(/tweetText/);
+			done();
+		});
+	})
+	.timeout(5000);
+
+
+	it('tweet', done =>
+	{
+		exec('bin/twitter-drone search tweet --query=test | bin/twitter-drone tweet --dry-run', (error, stdout, stderr) =>
+		{
+			expect(stderr).to.have.length;
+			done();
+		});
+	})
+	.timeout(5000);
+
+	it('retweet', done =>
+	{
+		exec('bin/twitter-drone search tweet --query=test | bin/twitter-drone retweet --dry-run', (error, stdout, stderr) =>
+		{
+			expect(stderr).to.have.length;
+			done();
+		});
+	})
+	.timeout(5000);
+
+	it('like', done =>
+	{
+		exec('bin/twitter-drone search tweet --query=test | bin/twitter-drone like --dry-run', (error, stdout, stderr) =>
+		{
+			expect(stderr).to.have.length;
+			done();
+		});
+	})
+	.timeout(5000);
+
+	it('follow', done =>
+	{
+		exec('bin/twitter-drone search tweet --query=test | bin/twitter-drone follow --dry-run', (error, stdout, stderr) =>
+		{
+			expect(stderr).to.have.length;
+			done();
+		});
+	})
+	.timeout(5000);
 });
